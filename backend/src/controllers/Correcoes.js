5612b98ce4f8c04a608666a980f36b635de9a45f
AleSchmidt <alexschmidt@hotmail.com>
2021-04-24 13:54:32 -0300

    ajuste bugfix e mensagens

--- backend/src/controllers/Correcoes.js
+++ backend/src/controllers/Correcoes.js
@@ -13,6 +13,8 @@ function setCorrecoes(req, res){
     const postCorrecoes = db.Correcoes.build({
       titulo: body.titulo || null,
       descricao: body.descricao || null,      
+      usuarioId: body.usuarioId || null,
+      arquivo: body.arquivo || null
     })
     postCorrecoes.save().then(() => {
       res.status(200).send({return: true, msg: 'Correcoes inseridas.'})
@@ -44,58 +46,25 @@ function getCorrecoes (req, res){
   let strBusca = ''  
   
   // console.log(res.locals.__sessEntreLinhasRefresh+ '---')
-
-  if(param.titulo){    
-    for(let i = 0; i < str.length; i++){
-      
-      strBusca = strBusca + '%' + str[i]
-      if(str.length == 1){
-        strBusca = strBusca + '%'
-      }
-    }        
-    // console.log(res['_sessEntreLinhasRefresh'] +  '---1111--')    
-    if(param.dtInicio || param.dtFim){      
-      if(param.dtInicio &&  param.dtFim){
-        db.Correcoes.findAll({
-          attributes: [
-            'id', 'titulo', ['CreatedAt', 'dtCriacao']            
-          ],
-          where:  {
-            titulo: { [Op.like]: `%${strBusca}%`},
-            createdAt: {
-              [Op.between]: [moment(param.dtInicio).format('yyyy-MM-DD'), moment(param.dtFim).format('yyyy-MM-DD')]
-            }
-          }, 
-          include: [{
-            model: db.Usuarios ,
-            attributes: [
-              'id'
-            ],
-            include: [{
-              model: db.Pessoas,
-              attributes: [
-                'nome_pessoa'
-              ]
-            }]
-          }]
-
-        }).then(dados => {                   
-          res.send({return: true, dados:dados, __sessEntreLinhasRefresh : res.locals.__sessEntreLinhasRefresh})
-        }).catch(err => {
-          console.log('mamama')
-          console.log(err)
-          res.status(400).send({return: false, msg: err})
-        })
-      }else{
-        res.status(400).send('Verifique os parâmetros informados.')
-      }
-    }else{
+ 
+  for(let i = 0; i < str.length; i++){
+    
+    strBusca = strBusca + '%' + str[i]
+    if(str.length == 1){
+      strBusca = strBusca + '%'
+    }
+  }            
+  if(param.dtInicio || param.dtFim){            
+    if(param.dtInicio &&  param.dtFim){
       db.Correcoes.findAll({
         attributes: [
-          'id', 'titulo', ['createdAt', 'dtCriacao']
+          'id', 'titulo', ['CreatedAt', 'dtCriacao']            
         ],
         where:  {
-          titulo: { [Op.like]: `%${strBusca}%`}          
+          titulo: { [Op.like]: `%${strBusca}%`},
+          createdAt: {
+            [Op.between]: [`${moment(param.dtInicio).format('yyyy-MM-DD')} 00:00:00`, `${moment(param.dtFim).format('yyyy-MM-DD')} 23:59:59`]
+          }
         }, 
         include: [{
           model: db.Usuarios ,
@@ -106,21 +75,50 @@ function getCorrecoes (req, res){
             model: db.Pessoas,
             attributes: [
               'nome_pessoa'
-            ]
+            ],
+            required: false
           }]
         }]
 
-      }).then(dados => {            
-        res.status(200).send({return: true, dados:dados, __sessEntreLinhasRefresh:res.locals.__sessEntreLinhasRefresh})
+      }).then(dados => {                   
+        res.send({return: true, dados:dados, __sessEntreLinhasRefresh : res.locals.__sessEntreLinhasRefresh})
       }).catch(err => {
+        console.log('mamama')
         console.log(err)
         res.status(400).send({return: false, msg: err})
       })
+    }else{
+      res.status(400).send('Verifique os parâmetros informados.')
     }
-  }else{    
-    res.status(200).send({return: false, msg:'Informe o titulo para busca'})
-  }
-  
+  }else{
+    db.Correcoes.findAll({
+      attributes: [
+        'id', 'titulo', ['createdAt', 'dtCriacao']
+      ],
+      where:  {
+        titulo: { [Op.like]: `%${strBusca}%`}          
+      }, 
+      include: [{
+        model: db.Usuarios ,
+        attributes: [
+          'id'
+        ],
+        include: [{
+          model: db.Pessoas,
+          attributes: [
+            'nome_pessoa'
+          ],
+          required: false
+        }]
+      }]
+
+    }).then(dados => {            
+      res.status(200).send({return: true, dados:dados, __sessEntreLinhasRefresh:res.locals.__sessEntreLinhasRefresh})
+    }).catch(err => {
+      console.log(err)
+      res.status(400).send({return: false, msg: err})
+    })
+  }  
 }
 
 function getDadosCorrecao(req, res){
